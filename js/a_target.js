13756954c152d9ce26bde477164921498b36dad2
xuan <362386736@qq.com>
2018-09-25 15:09:37 +0800

    修改a标签样式

---
+++ js/a_target.js
@@ -0,0 +1,24 @@
+$(function () {
+    if (!isMobile.any()) {
+        $("a").attr("target", "_blank");
+    };
+})
+
+// 判断是否是移动设备
+var isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i) ? true : false;
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i) ? true : false;
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i) ? true : false;
+    },
+    any: function () {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
+    }
+};
